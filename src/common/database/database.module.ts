0fa41494d9b0591737c3f2b958387b47d6ac0863
Youssouf <luffyeic@gmail.com>
2020-11-17 18:17:01 +0100

    Modif database module

--- src/common/database/database.module.ts
+++ src/common/database/database.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { databaseProvider } from './database.provider';
+
 
 @Module({
   providers: [...databaseProvider],
   exports: [...databaseProvider],
+  imports: [ConfigModule],
 })
 export class DatabaseModule {}
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import * as cookieParser  from 'cookie-parser';
 
 
 
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
   app.use(cookieParser());
+
   await app.listen(3000);
 }
 bootstrap();
